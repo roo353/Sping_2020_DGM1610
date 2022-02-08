63f06e324879004a81ff72012a4cf116965110a3
Kaitlyn Kinsey <kaitlynkinsey353@gmail.com>
2022-02-07 17:00:01 -0700

    Created calculator and learned float

---
+++ MyFirstGame/Assets/Scripts/Operators/SimpleCalc.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCalc : MonoBehaviour
+{
+
+    //Access Modifier DataType Name;
+    public int numberA = 10;
+    public int numberB = 33;
+    public int numberC = 7;
+
+    public float numberD = 33.0f;
+    public float numberE = 7.0f;
+
+    // + - * / % used for calculations 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Add();
+        Sub();
+        Mult();
+        Div();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Add()
+    {
+        int total;
+
+        total = numberA + numberB;
+
+        Debug.Log(numberA +" + "+ numberB +" = "+ total);
+    }
+
+    void Sub()
+    {
+        int total;
+
+        total = numberB - numberC;
+
+        Debug.Log(numberB +" - "+ numberC +" = "+ total);
+    }
+
+    void Mult()
+    {
+         int total;
+
+        total = numberB * numberB;
+
+        Debug.Log(numberB +" * "+ numberB +" = "+ total);
+    }
+
+    void Div()
+    {
+         int total;
+         int rem;
+         float tot;
+
+        total = numberB / numberC;
+        //can do below  
+        rem = numberB % numberC;
+        // or float
+        tot = numberD / numberE;
+
+        Debug.Log(numberB +" / "+ numberC +" = "+ total);
+        Debug.Log("Remainder ="+ rem);
+        Debug.Log(numberD +" / "+ numberE +" = "+ tot);
+    }
+}
