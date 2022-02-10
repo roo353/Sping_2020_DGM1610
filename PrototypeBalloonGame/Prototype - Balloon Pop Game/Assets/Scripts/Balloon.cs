87259d0962ba74331fcda1ff4c13c7d24c10bee6
Kaitlyn Kinsey <kaitlynkinsey353@gmail.com>
2022-02-09 17:12:49 -0700

    Created Prototype Balloon Game

---
+++ PrototypeBalloonGame/Prototype - Balloon Pop Game/Assets/Scripts/Balloon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+
+    public int scoreToGive = 100; // points balloons are worth
+    
+    public int clicksToPop = 3; // how many clicks till balloon pops
+    
+    public float scaleToIncrease = 0.10f; // how much the balloon scales per click
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        // reduces clicksToPop by 1 per click
+        clicksToPop -= 1; 
+
+        //increases scale of balloon per click by one
+        transform.localScale += Vector3.one * scaleToIncrease;
+
+        if(clicksToPop == 0)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
