6cb800cae8a298be44ebd595b0b52c5da7daab39
Kaitlyn Kinsey <kaitlynkinsey353@gmail.com>
2022-04-13 17:15:03 -0600

    Created RetroFPS

---
+++ PrototypeRoguelike/Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+
+    //public int numEwoks = 20;
+
+    //public int ducksInPond = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        /*for(int i = 1; i <= numEwoks; i++) //counts up, will loop till max set ewoks
+        {
+            Debug.Log("There are " + i + " Ewoks on a log."); //i is number
+        }
+        
+
+        while(ducksInPond > 0)
+        {
+            Debug.Log(ducksInPond);
+            Debug.Log("All of the ducks have flown south for the winter");
+            ducksInPond ++; //dont forget or else infinite loop ill occur
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PrototypeRoguelike/Retro FPS Clone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+    public float moveSpeed;
+    private Rigidbody rb;
+    private Vector3 moveInput;
+    private Vector2 mouseInput;
+    public float mouseSensitivity = 1.0f;
+
+    public Camera viewCam;
+    public GameObject bulletImpact;
+    public int currentAmmo;
+
+    private void Awake()
+    {
+      instance = this; 
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // Hide and lock cursor
+        Cursor.visible = false;
+        Cursor. lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      moveInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //getting inputs
+      // blends vectors (can move x and z at the same time)
+      Vector3 moveH = transform.right * moveInput.x;
+      Vector3 moveV = transform.forward * moveInput.z;
+      rb.velocity = (moveH + moveV) * moveSpeed; 
+
+      mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y") * mouseSensitivity);
+      transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y - mouseInput.x, transform.rotation.eulerAngles.z);
+
+      if(currentAmmo > 0)
+      {
+        Ray ray = viewCam.ViewportPointToRay(new Vector3(0.5f, 0.5f,0f));
+        RaycastHit hit;
+
+        if( Physics.Raycast(ray, out hit))
+        {
+          Debug.Log("Im looking " + hit.transform.name);
+          Instantiate(bulletImpact, hit.point, transform.rotation);
+        }
+        else
+        {
+          Debug.Log("I'm not looking at anything");
+        }
+      }
+    }
+}
